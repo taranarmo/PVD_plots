8a3bada8b621f8997d86d948dfee33fc3333fac3
Sergey <taranarmo@gmail.com>
2021-03-24 20:13:08 +0300

    add legend to last sublot, trim air recording, update plots

--- make_plots.py
+++ make_plots.py
@@ -11,15 +11,18 @@ def plot_pvd(x, y, ax=None):
         ax.plot(x.loc[:, depth], y.loc[:, depth], label=f"{depth:.2f}", color=color)
 
 
+index = slice("2020-03-28", "2020-04-05")
 aq1 = {}
 for direction in ["East", "North", "Up"]:
     aq1[direction.lower()] = pd.read_csv(f"AQ1_EXP102.{direction}.csv", index_col=0, parse_dates=True)
     aq1[direction.lower()].columns = aq1[direction.lower()].columns.astype(float)
+    aq1[direction.lower()] = aq1[direction.lower()].loc[index]
 
 aq2 = {}
 for direction in ["East", "North", "Up"]:
     aq2[direction.lower()] = pd.read_csv(f"AQ2_EXP1-202.{direction}.csv", index_col=0, parse_dates=True)
     aq2[direction.lower()].columns = aq1[direction.lower()].columns.astype(float)
+    aq2[direction.lower()] = aq2[direction.lower()].loc[index]
 
 cols = aq1['east'].columns.values[::10]
 fig, axes = plt.subplots(2, 2, figsize=(7, 5))
@@ -28,8 +31,9 @@ plot_pvd(aq1["east"], aq1["up"], axes[0])
 plot_pvd(aq1["north"], aq1["up"], axes[1])
 plot_pvd(aq1["east"], aq1["north"], axes[2])
 cell = np.argmin(np.abs((1.6 - aq1['east'].columns.values)))
-axes[3].plot(aq1["east"].iloc[:, cell].cumsum(), aq1["north"].iloc[:, cell].cumsum())
-axes[3].plot(aq2["east"].iloc[:, cell].cumsum(), aq2["north"].iloc[:, cell].cumsum())
+axes[3].plot(aq1["east"].iloc[:, cell].cumsum(), aq1["north"].iloc[:, cell].cumsum(), label="device1")
+axes[3].plot(aq2["east"].iloc[:, cell].cumsum(), aq2["north"].iloc[:, cell].cumsum(), label="device2")
+axes[3].legend()
 
 for ax, label in zip(axes, "abcd"):
     ax.text(s=label, x=0.05, y=0.5, transform=ax.transAxes)
