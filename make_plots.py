2dbc8b3af78ae71d33762a3b1e6261023efae688
Sergey <taranarmo@gmail.com>
2021-03-24 19:26:45 +0300

    finally make script work

--- make_plots.py
+++ make_plots.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-import matpotlib as plt
+import matplotlib.pyplot as plt
 
 
 def plot_pvd(x, y, ax=None):
-    if not ax:
+    if ax is None:
         ax = plt.gca()
     x, y = (data.cumsum() for data in (x, y))
     for depth, color in zip(cols, plt.cm.bwr_r(np.linspace(0, 1, len(cols)))):
@@ -22,8 +22,11 @@ for direction in ["East", "North", "Up"]:
     aq2[direction.lower()] = pd.read_csv(f"AQ2_EXP1-202.{direction}.csv", index_col=0, parse_dates=True)
     aq2[direction.lower()].columns = aq1[direction.lower()].columns.astype(float)
 
+cols = aq1['east'].columns.values[::10]
 fig, axes = plt.subplots(2, 2, figsize=(7, 5))
+axes = axes.flatten()
 plot_pvd(aq1["east"], aq1["up"], axes[0])
 plot_pvd(aq1["north"], aq1["up"], axes[1])
 plot_pvd(aq1["east"], aq1["north"], axes[2])
 plt.savefig("pvds.png")
+plt.show()
